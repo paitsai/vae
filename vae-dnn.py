e9670c4f97e79dac088dc31189e3dc867fad7c42
paitsai <chuijih.hsiao@gmail.com>
2024-07-27 21:02:24 +0800

    finish VAE on DNN of MNIST

---
+++ vae-dnn.py
@@ -0,0 +1,140 @@
+# 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
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
+
+
+# 我们选择使用DNN在MNIST数据集上进行实验
+
+latent_dim=2
+input_dim=28*28 # mnist数据集图像的像素
+
+inter_dim=256
+
+class VAE(nn.Module):
+    def __init__(self,input_dim=input_dim,inter_dim=inter_dim,latent_dim=latent_dim):
+        super(VAE, self).__init__()
+
+        # 第一部分编码器 encoder
+        self.encoder = nn.Sequential(
+            nn.linear(input_dim,inter_dim),
+            nn.ReLU(),
+            nn.Linear(inter_dim, 2*latent_dim)  # 2*latent_dim 因为我们需要返回两个值，一个是均值，一个是方差
+        )
+
+        self.decoder=nn.Sequential(
+            nn.linear(latent_dim, inter_dim),
+            nn.ReLU(),
+            nn.Linear(inter_dim, input_dim),
+            nn.Sigmoid()  # 最后一层用Sigmoid函数保证输出在0~1之间
+        )
+
+
+    def reparameterize(self,mu,logvar):
+        epsilon=torch.randn_like(mu)
+        return mu+torch.exp(logvar/2)*epsilon
+    
+    
+    def forward(self,x):
+        org_size = x.size()
+        batch = org_size[0]
+        x = x.view(batch, -1)
+
+        h = self.encoder(x)
+        mu, logvar = h.chunk(2, dim=1)
+        z = self.reparameterize(mu, logvar)
+        recon_x = self.decoder(z).view(size=org_size)
+
+        return recon_x, mu, logvar
+    
+
+kl_loss = lambda mu, logvar: -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+recon_loss = lambda recon_x, x: F.binary_cross_entropy(recon_x, x, size_average=False)
+
+
+epochs = 100
+batch_size = 128
+
+transform = transforms.Compose([transforms.ToTensor()])
+data_train = MNIST('MNIST_DATA/', train=True, download=True, transform=transform)
+data_valid = MNIST('MNIST_DATA/', train=False, download=True, transform=transform)
+
+train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=0)
+test_loader = DataLoader(data_valid, batch_size=batch_size, shuffle=False, num_workers=0)
+
+
+
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = VAE(input_dim, inter_dim, latent_dim)
+model.to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+best_loss = 1e9
+best_epoch = 0
+
+valid_losses = []
+train_losses = []
+
+for epoch in range(epochs):
+    print(f"Epoch {epoch}")
+    model.train()
+    train_loss = 0.
+    train_num = len(train_loader.dataset)
+
+    for idx, (x, _) in enumerate(train_loader):
+        batch = x.size(0)
+        x = x.to(device)
+        recon_x, mu, logvar = model(x)
+        recon = recon_loss(recon_x, x)
+        kl = kl_loss(mu, logvar)
+
+        loss = recon + kl
+        train_loss += loss.item()
+        loss = loss / batch
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if idx % 100 == 0:
+            print(f"Training loss {loss: .3f} \t Recon {recon / batch: .3f} \t KL {kl / batch: .3f} in Step {idx}")
+
+    train_losses.append(train_loss / train_num)
+
+    valid_loss = 0.
+    valid_recon = 0.
+    valid_kl = 0.
+    valid_num = len(test_loader.dataset)
+    model.eval()
+    with torch.no_grad():
+        for idx, (x, _) in enumerate(test_loader):
+            x = x.to(device)
+            recon_x, mu, logvar = model(x)
+            recon = recon_loss(recon_x, x)
+            kl = kl_loss(mu, logvar)
+            loss = recon + kl
+            valid_loss += loss.item()
+            valid_kl += kl.item()
+            valid_recon += recon.item()
+
+        valid_losses.append(valid_loss / valid_num)
+
+        print(f"Valid loss {valid_loss / valid_num: .3f} \t Recon {valid_recon / valid_num: .3f} \t KL {valid_kl / valid_num: .3f} in epoch {epoch}")
+
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            best_epoch = epoch
+
+            torch.save(model.state_dict(), 'best_model_mnist')
+            print("Model saved")
+
+
